c49ad4550872baaa61bc876e3cb89b3055899249
Ethnarch <rbenefoz@gmail.com>
2017-02-03 13:35:39 -0500

    first commit

---
+++ Desktop/mysite/polls/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+app_name = 'polls'
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name = 'index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
